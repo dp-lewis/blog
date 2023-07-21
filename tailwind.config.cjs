923ad5315422cd07b3edf169d671c716f7426be3
David Lewis <mail@david-lewis.com>
2023-07-21 16:49:17 +1000

    Fix up home layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,11 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		fontFamily: {
-			display: ["Lilita One"],
-		},
-		height: {
-			'screen-half': '50vh',
+		extend: {
+			fontFamily: {
+				display: ["Lilita One"],
+			},
+			height: {
+				'screen-half': '50vh',
+			}
 		}
 	},
 	plugins: [
